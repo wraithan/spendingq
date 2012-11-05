72ad70d4d2b18fe026b763f78577d536eda4fa15
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2012-11-05 10:35:45 -0800

    jquery :(

--- spendingq/core/static/js/home.js
+++ spendingq/core/static/js/home.js
@@ -1,25 +1,17 @@
-var form = ''
-
+var handleForm = function(form) {
+}
 
 var handleSubmit = function() {
-    var httpRequest = new XMLHttpRequest();
-
-    httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState === 4) {
-            if (httpRequest.status === 200) {
-                alert(httpRequest.responseText)
-            } else {
-                alert('There was a problem with the request.')
-            }
-        }
-    }
-    httpRequest.open('GET', form.dataset.submitUrl + '?format=json')
-    httpRequest.send(null)
+    var form = $('inputForm')
+    $.get(form.data('submitUrl'),
+          {format: 'json'},
+          function(data) {
+              alert(data)
+          })
 }
 
 var onLoad = function() {
     document.getElementById('submit').addEventListener('click', handleSubmit)
-    form = document.getElementById('inputForm')
 }
 
 document.addEventListener('DOMContentLoaded', function() {onLoad()})
