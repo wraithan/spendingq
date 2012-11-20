94cadbd0c3ba0a7bd0df0bbc00b03a71d140df52
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2012-11-20 10:34:05 -0800

    raven/sentry

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py run_gunicorn -w 9 -b 0.0.0.0:$PORT
+web: newrelic-admin run-program python manage.py run_gunicorn -w 9 -b 0.0.0.0:$PORT
--- spendingq/settings/prod.py
+++ spendingq/settings/prod.py
@@ -1,6 +1,11 @@
+from os import getenv
+
 from .base import *
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 STATIC_URL = 'http://media.spendingq.com/static/'
 SITE_URL = 'spendingq.com'
+SENTRY_DSN = getenv('SENTRY_DSN', None)
+
+INSTALLED_APPS += ('raven.contrib.django',)
